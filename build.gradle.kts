b6a2f6cc8db273e4297f83d35b3be3ea26dfb3b2
fumtas1k <fumta01a7@gmail.com>
2022-10-16 13:56:31 +0900

    [add]コルーチン追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,30 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
-    application
+  kotlin("jvm") version "1.7.20"
+  application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+  testImplementation(kotlin("test"))
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+  kotlinOptions.jvmTarget = "1.8"
 }
 
 application {
-    mainClass.set("MainKt")
+  mainClass.set("MainKt")
 }
